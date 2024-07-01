5188c74b4465ec5ac109477ac194063015eb5671
Volodymyr Smoliak <svovka83@gmail.com>
2024-07-01 14:09:05 +0300

    commit

--- src/pages/FramerMotion/MotionTree/MotionTree.jsx
+++ src/pages/FramerMotion/MotionTree/MotionTree.jsx
@@ -1,10 +1,11 @@
 import { motion } from "framer-motion";
 
 import styles from "./MotionTree.module.css";
+
 const MotionTree = () => {
   return (
     <div className={styles.motionTree}>
-      <img src="DSC00213.JPG" alt="motionTree" />
+      <img src="./DSC00213.JPG" alt="motionTree" />
     </div>
   );
 };
